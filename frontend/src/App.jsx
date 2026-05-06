96f861867292d879d0801a6d9e73cd20bb579d6c
Shaima Sharfuddin <shaimasharfuddin654@gmail.com>
2026-05-06 12:10:34 +0530

    fix router context issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
